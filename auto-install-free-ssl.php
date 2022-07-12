e7578f5f9a6188bd3678b5b92d7211040afe212a
Josh Hartman <josh@warpconduit.net>
2022-07-12 14:08:24 -0500

    Disable plugin updates

--- auto-install-free-ssl.php
+++ auto-install-free-ssl.php
@@ -10,6 +10,7 @@
  * Author URI:  https://freessl.tech
  * License:     GNU General Public License, version 3
  * License URI: http://www.gnu.org/licenses/gpl-3.0.html
+ * Update URI:  false
  * Text Domain: auto-install-free-ssl
  * Domain Path: /languages/
  * Network:     true
